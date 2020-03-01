f49230b8ae709f9d438cda610fb49b627c770bf4
qiaowei1996  <965184371@qq.com>
2020-03-01 13:31:09 +0800

    python 基础

---
+++ mytest.py
@@ -0,0 +1,48 @@
+# a=1
+# b=2.5
+# c="123"
+# print(a,b,c)
+#
+# print(type(a))
+#
+# print("a={},b={}".format(a,b))
+
+
+#
+# a=[1,2,3,4,5,6,7,8,9]
+#
+# print(a[-3])
+# print(a[3:8:2])
+
+# a=list(range(1,999+1))
+# print(a[1::2])
+
+
+# 倒叙
+a=[1,2,3,4,5,6,10,8,9,7,10]
+b=[0,1,2,3]
+# a.reverse()
+# a.sort()
+# b=sorted(a)
+# a.append(a[3])
+# a.insert(1,5)
+# a.extend(b)
+# b.pop(3)
+# a.remove(3)
+# c=a.count(10)
+# c=max(a)
+# c=len(a)
+# del a[1]
+# c=a.index(3)
+# a.clear()
+
+
+# print(a)
+
+
+
+a=2
+# a*=3+3
+a*=6
+
+print(a)
